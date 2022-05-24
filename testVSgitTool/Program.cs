a9cd1e0bea1331d88afaee7b757829b026afd194
Alson-Pchome <alsonchang@pchome.tw>
2022-05-24 14:54:38 +0800

    測試

---
+++ testVSgitTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testVSgitTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
